03a56864f5b2f5895c7eec6584154dff8f5c3e5e
Wildan Mubarok <willnode@wellosoft.net>
2019-08-07 01:30:59 +0700

    cilok

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pip
-with open("requirements.txt") as f:
+with open("real-requirements.txt") as f:
     for line in f:
         # call pip's main function with each requirement
         pip.main(['install','-U', line])
