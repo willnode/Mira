4115fc793c1a154be2d5a1fa3708ea174c8adf52
Wildan Mubarok <willnode@wellosoft.net>
2019-08-07 01:40:20 +0700

    flag up

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 import pip
-with open("real-requirements.txt") as f:
+
+reqs = """wheel==0.30.0
+setuptools==39.0.1
+pip==9.0.1
+gensim
+tensorflow
+line-bot-sdk
+scikit-learn
+numpy
+flask
+mysql
+mysql-connector-python-rf""".splitlines(False)
+
+with reqs as f:
     for line in f:
 	    print line
         # call pip's main function with each requirement
