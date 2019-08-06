d5e766b98a80c39681a883da1edbd3f0fa4bf880
Wildan Mubarok <willnode@wellosoft.net>
2019-08-07 01:37:37 +0700

    chilo

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import pip
 with open("real-requirements.txt") as f:
     for line in f:
+		print line
         # call pip's main function with each requirement
         pip.main(['install','-U', line])
